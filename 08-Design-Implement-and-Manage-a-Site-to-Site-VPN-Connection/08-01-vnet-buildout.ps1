c356072341b8bee8e58ec0e1be578f61b197c3a3
Marcos Monteiro <marcos-monteiro@pluralsight.com>
2024-10-28 17:40:35 +1100

    Update subnet prefix

--- 08-Design-Implement-and-Manage-a-Site-to-Site-VPN-Connection/08-01-vnet-buildout.ps1
+++ 08-Design-Implement-and-Manage-a-Site-to-Site-VPN-Connection/08-01-vnet-buildout.ps1
@@ -24,7 +24,6 @@ $location = az group list --query '[].location' -o tsv
 ##### END - VARIABLES ######
 ##############################
 
-
 ##############################
 ####### START - SCRIPT #######
 ##############################
@@ -33,18 +32,15 @@ $location = az group list --query '[].location' -o tsv
 
 az network vnet create --name hub-vnet --resource-group $rg --location $location --address-prefixes 10.0.0.0/16 --subnet-name hub-subnet-a --subnet-prefix 10.0.1.0/24 --subnet-name GatewaySubnet --subnet-prefix 10.0.2.0/27
 
-az network vnet create --name spoke1-vnet --resource-group $rg --location $location --address-prefixes 10.1.0.0/16 --subnet-name spoke-1-subnet-a --subnet-prefix 10.1.1.0/24 --subnet-name GatewaySubnet --subnet-prefix 10.1.2.0/27
+az network vnet create --name spoke1-vnet --resource-group $rg --location $location --address-prefixes 10.1.0.0/16 --subnet-name spoke-1-subnet-a --subnet-prefix 10.1.0.0/24 --subnet-name GatewaySubnet --subnet-prefix 10.1.1.0/27
 
 # Create two Linux machines. One in each network
 
 az vm create --resource-group $rg --name spoke-1-vm --image Ubuntu2204 --generate-ssh-keys --public-ip-address myPublicIP-spoke-1-vm --public-ip-sku Standard --vnet-name spoke1-vnet --subnet spoke-1-subnet-a --size Standard_B1s
 
-
 az vm create --resource-group $rg --name hub-vm --image Ubuntu2204 --generate-ssh-keys --public-ip-address myPublicIP-nva --public-ip-sku Standard --vnet-name hub-vnet --subnet hub-subnet-a --size Standard_B1s
-
 
 # Add rules to default NIC NSGs to allow ICMP
 az network nsg rule create --resource-group $rg --nsg-name hub-vmNSG --name allowIcmp --priority 110 --destination-port-ranges 0-65535 --access Allow --protocol Icmp
 
 az network nsg rule create --resource-group $rg --nsg-name spoke-1-vmNSG --name allowIcmp --priority 110 --destination-port-ranges 0-65535 --access Allow --protocol Icmp
-
